f9fae6a251eda94c0167f10f31c44fd52883e010
Prasanna Kumar Reddi <prasanna.reddi@apxor.com>
2023-06-25 01:16:21 +0530

    Adds Data class that is used as the object of communication

---
+++ Data.java
@@ -0,0 +1,14 @@
+public class Data {
+    private String name;
+    private String type;
+    Data(String name,String type) {
+        this.name = name;
+        this.type = type;
+    }
+    public String getName() {
+        return this.name;
+    }
+    public String getType() {
+        return this.type;
+    }
+}
